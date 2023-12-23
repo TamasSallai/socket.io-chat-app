3b6e7445710d60b3ffeafffd7962af0e568820e4
Sallai Tamás <tamas.sallai.hu@gmail.com>
2023-12-23 20:45:31 +0100

    sidebar design

--- client/src/components/ChatLayout/Sidebar/Sidebar.tsx
+++ client/src/components/ChatLayout/Sidebar/Sidebar.tsx
@@ -1,7 +1,26 @@
+import SidebarHeader from './SidebarHeader/SidebarHeader'
 import './Sidebar.css'
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>
+  return (
+    <div className="sidebar">
+      <SidebarHeader />
+
+      <ul className="chatrooms">
+        <li className="chatroom-entry">
+          <img src="./profile-picture.jpg" alt="" />
+          <div>
+            <div className="chatroom-name">Tamas Sallai</div>
+            <p className="last-message">Lorem ipsum dolor sit.</p>
+          </div>
+          <div>
+            <div className="message-counter">1</div>
+            <span></span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Sidebar
